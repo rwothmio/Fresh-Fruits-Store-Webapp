91fc3a5766ca579ea6c50a723e79e6b7b5e84aa4
rwothmio <brwothmio@gmail.com>
2023-08-11 16:45:35 +0300

    convert query and dataset to lowercase

--- src/containers/search-page/SearchPage.jsx
+++ src/containers/search-page/SearchPage.jsx
@@ -4,27 +4,39 @@ import { useSearchParams } from "react-router-dom";
 import getAllFruits from "../../utility-functions/getAllFruits";
 import ProductListing from "../../components/product-listing/ProductListing";
 
-
-const allFruits = getAllFruits()
+const allFruits = getAllFruits();
 
 function SearchPage() {
-  const [query] = useSearchParams()
-  const q = query.get('q')
-  const searchResults = allFruits.filter(fruit => fruit.name.includes(q) || fruit.category.includes(q))
+  const [query] = useSearchParams();
+  const q = query.get("q");
+
+  const searchResults = allFruits.filter((fruit) => {
+    const convertedName = fruit.name.toLowerCase();
+    const convertedDescr = fruit.name.toLowerCase()
+    const convertedQuery = q.toLowerCase()
+
+    return convertedName.includes(convertedQuery) || convertedDescr.includes(convertedQuery);
+
+  });
   return (
     <div className="searchPage">
-      {
-        searchResults.length === 0 
-
-        ? <p>no results for <strong className="query">{q}</strong></p> 
-
-        :<>
-          <h2>{searchResults.length} results for <strong className="query">{q}</strong></h2>
+      {searchResults.length === 0 ? (
+        <p>
+          no results for <strong className="query">{q}</strong>
+        </p>
+      ) : (
+        <>
+          <h2>
+            {searchResults.length} results for{" "}
+            <strong className="query">{q}</strong>
+          </h2>
           <div className="searchPage__results">
-        {searchResults.map(result => <ProductListing key={result.id} fruit={result} />)}
-        </div>
+            {searchResults.map((result) => (
+              <ProductListing key={result.id} fruit={result} />
+            ))}
+          </div>
         </>
-      }
+      )}
     </div>
   );
 }

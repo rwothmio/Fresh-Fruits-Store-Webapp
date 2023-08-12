26e05187efa7004b0c4222c6779a0286ef760748
rwothmio <brwothmio@gmail.com>
2023-08-12 12:37:52 +0300

    add index.css file

--- App.jsx
+++ App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./app.css";
+import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./src/components/store/index";
